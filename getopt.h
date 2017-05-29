0895a7696d73112fdd16171cf028e8df2fc2b744
Glenn Maynard <glenn@zewt.org>
2017-05-28 21:28:59 -0500

    Reformat code.  2-space indents are completely unreadable.

--- getopt.h
+++ getopt.h
@@ -5,24 +5,24 @@
 extern "C" {
 #endif
 
-extern const int no_argument;
-extern const int required_argument;
-extern const int optional_argument;
+    extern const int no_argument;
+    extern const int required_argument;
+    extern const int optional_argument;
 
-extern char* optarg;
-extern int optind, opterr, optopt;
+    extern char* optarg;
+    extern int optind, opterr, optopt;
 
-struct option {
-  const char* name;
-  int has_arg;
-  int* flag;
-  int val;
-};
+    struct option {
+	const char* name;
+	int has_arg;
+	int* flag;
+	int val;
+    };
 
-int getopt(int argc, char* const argv[], const char* optstring);
+    int getopt(int argc, char* const argv[], const char* optstring);
 
-int getopt_long(int argc, char* const argv[],
-  const char* optstring, const struct option* longopts, int* longindex);
+    int getopt_long(int argc, char* const argv[],
+	const char* optstring, const struct option* longopts, int* longindex);
 
 #if defined(__cplusplus)
 }
