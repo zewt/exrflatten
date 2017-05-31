c6e0e1d39c8830976d84ab59f0d20d0da406c453
Glenn Maynard <glenn@zewt.org>
2017-05-30 21:28:26 -0500

    This operation doesn't require normals.

--- DeepImageStroke.cpp
+++ DeepImageStroke.cpp
@@ -530,7 +530,6 @@ EXROperation_Stroke::EXROperation_Stroke(const SharedConfig &sharedConfig_, stri
 void EXROperation_Stroke::AddChannels(shared_ptr<DeepImage> image, DeepFrameBuffer &frameBuffer) const
 {
     image->AddChannelToFramebuffer<V3f>("P", frameBuffer, false);
-    image->AddChannelToFramebuffer<V3f>("N", frameBuffer, false);
 
     if(!strokeDesc.strokeMaskChannel.empty())
 	image->AddChannelToFramebuffer<float>(strokeDesc.strokeMaskChannel, frameBuffer, true);
